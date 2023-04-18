e327b811dc9d5a7b875208a8dc2a5de8783c4491
Viktor Biruk <vikttur.job@gmail.com>
2023-04-18 23:58:34 +0300

    Performs Task 1 - color switch

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+	disabledBtn(true);
+	startTimer();
+});
+
+stopBtn.addEventListener("click", () => {
+	clearInterval(timerId);
+	disabledBtn(false);
+});
+
+function disabledBtn(isDisabled) {
+	startBtn.disabled = isDisabled;
+};
+
+function startTimer() {
+		timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
 
